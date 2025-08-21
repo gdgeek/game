a0b1cfd94e6a31ff1fcda7efc6a247e6771ead8f
gdgeek <dirui1@yeah.net>
2025-08-21 16:01:08 +0800

    no message

--- api/modules/v2/controllers/ServerController.php
+++ api/modules/v2/controllers/ServerController.php
@@ -128,12 +128,19 @@ class ServerController extends Controller
         return [
             'money' => 0,
             [
+                'slogans' => [
+                    '我在这里很想你',
+                    '今天也要加油鸭',
+                    '阳光正好，微风不燥',
+                    '记录每一刻，热爱每一天'
+                ],
                 'pictures' => [
                     'https://game-1251022382.cos.ap-nanjing.myqcloud.com/picture/t1.png',
                     'https://game-1251022382.cos.ap-nanjing.myqcloud.com/picture/t2.png',
                     'https://game-1251022382.cos.ap-nanjing.myqcloud.com/picture/t3.png',
                     'https://game-1251022382.cos.ap-nanjing.myqcloud.com/picture/t4.png',
                 ],
+
                 'shot' => [1, 5, 10, 20],
             ],
         ];
